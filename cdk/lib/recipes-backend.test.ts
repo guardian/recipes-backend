e8916f06999a96d537c8098829b9fae47501e8d0
Andy Gallagher <fredex42@gmail.com>
2023-11-01 14:59:19 +0000

    initial boilerplate

---
+++ cdk/lib/recipes-backend.test.ts
@@ -0,0 +1,12 @@
+import { App } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { RecipesBackend } from "./recipes-backend";
+
+describe("The RecipesBackend stack", () => {
+  it("matches the snapshot", () => {
+    const app = new App();
+    const stack = new RecipesBackend(app, "RecipesBackend", { stack: "content-api", stage: "TEST" });
+    const template = Template.fromStack(stack);
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+});
