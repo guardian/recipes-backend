c2ddeabdcdd9d2390591c4b54adf922a33d92718
Andy Gallagher <fredex42@gmail.com>
2023-11-01 15:22:38 +0000

    remove boilerplate node project, add CDK into NX and set up base build-scripts

--- cdk/lib/recipes-backend.ts
+++ cdk/lib/recipes-backend.ts
@@ -1,9 +1,14 @@
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 export class RecipesBackend extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    new Bucket(this, "StaticHosting", {
+
+    });
   }
 }
