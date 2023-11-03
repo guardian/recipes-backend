a85cd4ce8cea541453796f71e75a58b9ea36c48d
Andy Gallagher <fredex42@gmail.com>
2023-11-03 10:06:12 +0000

    add table name via env var to test-data script

--- tools/fill-db/src/main.ts
+++ tools/fill-db/src/main.ts
@@ -3,7 +3,7 @@ import { RecipeIndexEntry, RecipeIndexEntryToDynamo } from "./models";
 import {v4 as uuid} from "uuid";
 import formatISO from "date-fns/formatISO";
 
-const tableName = "";
+const tableName = process.env["TABLE_NAME"];
 const client = new DynamoDBClient();
 const limit = 30000;
 
@@ -40,6 +40,11 @@ async function batchWrite(records:Record<string, AttributeValue>[]) {
 }
 
 //START MAIN
+if(!tableName || tableName=="") {
+  console.log(`ERROR You need to set the TABLE_NAME variable to the table to write.  This table will be filled with approximately ${limit} dummy records.`);
+  process.exit(1);
+}
+
 let recipes = 0;
 while(recipes < limit) {
   const countForArticle = Math.ceil(Math.random() * 5);
