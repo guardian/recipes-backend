0f4fe324dbe5faf0c9828c962146aa0cb6af3d0f
Divya Bhatt <divya.bhatt@guardian.co.uk>
2023-11-17 13:43:40 +0000

    Completing the function. Not tested yet.

--- lib/recipes-data/src/lib/extract-all-recipes.ts
+++ lib/recipes-data/src/lib/extract-all-recipes.ts
@@ -3,19 +3,29 @@ import type {Content} from "@guardian/content-api-models/v1/content";
 import {ContentType} from "@guardian/content-api-models/v1/contentType";
 import {extractRecipeData} from "./extract_recipedata_from_element";
 
-
-interface AllRecipesInAnArticle {
+export interface AllRecipesInAnArticle { //TODO confirm all these values to go in from article and recipe
   immutableID: string;
   mutableID: string;
   canonicalID: string;
 }
 
-export function extractAllRecipesFromArticle(content: Content): void { //TODO replace return type void with AllRecipesInAnArticle[]
+export function extractAllRecipesFromArticle(content: Content): AllRecipesInAnArticle[] {
+  let allRecipesInAnArticle = Array<AllRecipesInAnArticle>()
+
   if (content.type == ContentType.ARTICLE) {
     const articleBlocks: Blocks = content.blocks
     const getAllMainBlockRecipesIfPresent = extractRecipeData(content.id, articleBlocks.main)
-    const getAllBodyBlocksRecipesIfPresent = articleBlocks.body?.map(bodyBlock => extractRecipeData(content.id, bodyBlock))
-    const allRecipeFound = [...getAllMainBlockRecipesIfPresent, ...getAllBodyBlocksRecipesIfPresent]
+    const getAllBodyBlocksRecipesIfPresent = articleBlocks.body?.flatMap(bodyBlock => extractRecipeData(content.id, bodyBlock))
+    const recipesFound = getAllMainBlockRecipesIfPresent.concat(getAllBodyBlocksRecipesIfPresent)
+
+    allRecipesInAnArticle = recipesFound.map(recp => {
+      return <AllRecipesInAnArticle>{ //TODO to confirm as mentioned above as well
+        immutableID: recp.recipeUID,
+        mutableID: "",
+        canonicalId: content.id
+      }
+    })
   }
 
+  return allRecipesInAnArticle
 }
