5ef5090d049b5409339ef4bc18983e2da1abe77d
Andy Gallagher <fredex42@gmail.com>
2023-11-21 12:27:57 +0000

    remove the superceded recipes, too

--- lib/recipes-data/src/lib/takedown.ts
+++ lib/recipes-data/src/lib/takedown.ts
@@ -53,14 +53,14 @@ export async function removeAllRecipesForArticle(client: DynamoDBClient, canonic
  * currently present.  If we are missing any of the "current" recipes then these should be taken down.
  * @param dynamoClient DynamoDB client so we can query the index database
  * @param canonicalArticleId ID of the article that's being updated
- * @param recipeIdsToKeep list of the "new" recipes that are in the update (and should therefore be kept)
+ * @param recipeChecksumsToKeep list of the "new" recipes that are in the update (and should therefore be kept)
  * @return list of the recipes that were present in the current version but not in the update. These should be taken down.
  */
-export async function recipesToTakeDown(dynamoClient:DynamoDBClient, canonicalArticleId:string, recipeIdsToKeep: string[]):Promise<RecipeIndexEntry[]>
+export async function recipesToTakeDown(dynamoClient:DynamoDBClient, canonicalArticleId:string, recipeChecksumsToKeep: string[]):Promise<RecipeIndexEntry[]>
 {
-  const toKeepSet = new Set(recipeIdsToKeep);
+  const toKeepSet = new Set(recipeChecksumsToKeep);
   const currentSet = await recipesforArticle(dynamoClient, canonicalArticleId);
 
   //ES6 does not give us a Set.difference method, unfortunately. So we have to do it here.
-  return currentSet.filter(rec=>!toKeepSet.has(rec.recipeUID));
+  return currentSet.filter(rec=>!toKeepSet.has(rec.checksum));
 }
