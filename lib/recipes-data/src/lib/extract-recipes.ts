eeb0711bd4f7f8e122a4aae59d0bb1bf3d8d24b2
Andy Gallagher <fredex42@gmail.com>
2023-11-21 17:53:28 +0000

    make manual-takedown work again

--- lib/recipes-data/src/lib/extract-recipes.ts
+++ lib/recipes-data/src/lib/extract-recipes.ts
@@ -1,3 +1,4 @@
+import {createHash} from "crypto";
 import type {Block} from "@guardian/content-api-models/v1/block";
 import type {Blocks} from "@guardian/content-api-models/v1/blocks";
 import type {Content} from "@guardian/content-api-models/v1/content";
@@ -5,7 +6,6 @@ import {ContentType} from "@guardian/content-api-models/v1/contentType";
 import {ElementType} from "@guardian/content-api-models/v1/elementType";
 import {registerMetric} from "@recipes-api/cwmetrics";
 import type {RecipeReferenceWithoutChecksum} from './models';
-import {createHash} from "crypto";
 
 export async function extractAllRecipesFromArticle(content: Content): Promise<RecipeReferenceWithoutChecksum[]> {
   if (content.type == ContentType.ARTICLE && content.blocks) {
