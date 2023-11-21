7452a483719ac98d257abde73e14eff089a62133
Andy Gallagher <fredex42@gmail.com>
2023-11-21 13:59:12 +0000

    "real" data was in a subtly different format to what i expected

--- lib/recipes-data/src/lib/extract-recipes.ts
+++ lib/recipes-data/src/lib/extract-recipes.ts
@@ -5,6 +5,7 @@ import {ContentType} from "@guardian/content-api-models/v1/contentType";
 import {ElementType} from "@guardian/content-api-models/v1/elementType";
 import {registerMetric} from "@recipes-api/cwmetrics";
 import type {RecipeReferenceWithoutChecksum} from './models';
+import {createHash} from "crypto";
 
 export async function extractAllRecipesFromArticle(content: Content): Promise<RecipeReferenceWithoutChecksum[]> {
   if (content.type == ContentType.ARTICLE && content.blocks) {
@@ -28,13 +29,32 @@ export function extractRecipeData(canonicalId: string, block: Block): Array<Reci
     .map(recp => parseJsonBlob(canonicalId, recp.recipeTypeData?.recipeJson as string))
 }
 
+/**
+ * Most recipes have a UUID-style `id` field, so we pass that through.
+ * However some of the ones that were extracted by D&I have a numeric field still.
+ * In that case, we concatenate the canonical ID onto the numeric value and then sha1 the lot.
+ * @param contentIdField
+ * @param canonicalId
+ */
+function determineRecipeUID(contentIdField:string, canonicalId: string): string
+{
+  if(contentIdField.match(/^\d+$/)) {
+    const hasher = createHash("sha1");
+    //do the same as https://github.com/guardian/flexible-content/blob/6e963d9027d02a4f3af4637dbe6498934d904a4f/flexible-content-integration/src/main/scala/com/gu/flexiblecontent/integration/dispatcher/RecipesImportDispatcher.scala#L213
+    const stringToHash = `${contentIdField}-${canonicalId}`;
+    return hasher.update(stringToHash).digest("hex");
+  } else {
+    return contentIdField;
+  }
+}
+
 function parseJsonBlob(canonicalId: string, recipeJson: string): RecipeReferenceWithoutChecksum | null {
   const recipeData = JSON.parse(recipeJson) as Record<string, unknown>
   if (!recipeData.id) {
     return null
   } else {
     return <RecipeReferenceWithoutChecksum>{
-      recipeUID: `${recipeData.id as string}`,
+      recipeUID: determineRecipeUID(recipeData.id as string, canonicalId),
       jsonBlob: recipeJson
     }
   }
