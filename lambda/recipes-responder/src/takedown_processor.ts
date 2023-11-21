eb34fe0992a34ed81d0d57f0fa0a42b8c3c97961
Andy Gallagher <fredex42@gmail.com>
2023-11-21 18:32:19 +0000

    fix takedown handling

--- lambda/recipes-responder/src/takedown_processor.ts
+++ lambda/recipes-responder/src/takedown_processor.ts
@@ -1,26 +1,13 @@
 import type {DeletedContent} from "@guardian/content-api-models/crier/event/v1/deletedContent";
 import type {Event} from "@guardian/content-api-models/crier/event/v1/event"
-import {ContentType} from "@guardian/content-api-models/v1/contentType";
-import {awaitableDelay, removeAllRecipesForArticle} from "@recipes-api/lib/recipes-data";
+import {removeAllRecipesForArticle} from "@recipes-api/lib/recipes-data";
 import {DynamoClient} from "./dynamo_conn";
 
-export async function handleTakedown(evt:Event, attempt?:number):Promise<number> {
-  if(evt.payload?.kind=="content") {  //we don't need to handle if it's not an article
-    const content = evt.payload.content;
-    if(content.type==ContentType.ARTICLE) {  //we don't need to handle if it's not an article
-      try {
-        return removeAllRecipesForArticle(DynamoClient, content.id);
-      } catch(err) {
-        if( (attempt ?? 0) > 10) {
-          throw new Error(`Could not remove recipes for taken-down article ${content.id} after 10 attempts, giving up`);
-        }
-        console.error(`WARNING: Could not remove recipes for taken down article ${content.id}: `, err);
-        await awaitableDelay();
-        return handleTakedown(evt, (attempt ?? 0)+1)
-      }
-    }
-  }
-  return 0; //no action => nothing removed
+export async function handleTakedown(evt:Event):Promise<number> {
+  console.log("takedown payload: ", JSON.stringify(evt));
+
+  //there's no payload in the takedown message!
+  return removeAllRecipesForArticle(DynamoClient, evt.payloadId); //evt.payloadId is the canonical article ref that was taken down
 }
 
 // I don't think that these are relevant to us here. So, I'm logging it out to verify that suspicion
