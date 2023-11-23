dc9a13c3fa9ed12e32b0c89c3a21c17cefaf27cb
Andy Gallagher <fredex42@gmail.com>
2023-11-23 18:01:36 +0000

    validate that incoming data is json

--- lambda/rest-endpoints/src/app.ts
+++ lambda/rest-endpoints/src/app.ts
@@ -12,17 +12,32 @@ router.use(bodyParser.json());
 
 router.post('/api/curation', (req, resp)=>{
   const buffer = req.body as Buffer;
-  console.log(buffer.toString('utf-8'));
 
-  importNewData(buffer)
-    .then(()=>{
-      return resp.status(200).json({status: "ok"})
-    })
-    .catch((err)=>{
-      console.error(err);
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment -- can't help `err` being untyped
-      return resp.status(200).json({status: "error", detail: err.toString()})
-    })
+  if(req.header("Content-Type") != "application/json") {
+    resp.status(405).json({status: "error", detail: "wrong content type"});
+    return;
+  }
+
+  if(buffer.length==0) {
+    resp.status(400).json({status: "error", detail: "no content was sent"});
+    return;
+  }
+
+  try {
+    JSON.parse(buffer.toString('utf-8'));
+    importNewData(buffer)
+      .then(() => {
+        return resp.status(200).json({status: "ok"})
+      })
+      .catch((err) => {
+        console.error(err);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment -- can't help `err` being untyped
+        return resp.status(200).json({status: "error", detail: err.toString()})
+      })
+  } catch(err) {
+    console.error("Could not parse incoming data as json: ", err);
+    return resp.status(400).json({status: "error", detail: "invalid content"});
+  }
 });
 
 app.use('/', router);
