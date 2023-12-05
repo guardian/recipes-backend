39841eb4ba7fab2070732a8328e34e6bb7570b0d
Divya Bhatt <divya.bhatt@guardian.co.uk>
2023-12-05 14:38:09 +0000

    added alarm for Duration metric on recipe-responder lambda.

--- cdk/lib/recipes-backend.ts
+++ cdk/lib/recipes-backend.ts
@@ -3,11 +3,14 @@ import {GuParameter, GuStack} from "@guardian/cdk/lib/constructs/core";
 import {GuLambdaFunction} from "@guardian/cdk/lib/constructs/lambda";
 import {GuKinesisLambdaExperimental} from "@guardian/cdk/lib/experimental/patterns";
 import {StreamRetry} from "@guardian/cdk/lib/utils/lambda";
-import {type App, Duration} from "aws-cdk-lib";
+import {type App, aws_sns, Duration} from "aws-cdk-lib";
+import {Alarm, ComparisonOperator, TreatMissingData, Unit} from "aws-cdk-lib/aws-cloudwatch";
+import {SnsAction} from "aws-cdk-lib/aws-cloudwatch-actions";
 import {Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 import {Architecture, Runtime} from "aws-cdk-lib/aws-lambda";
 import {DataStore} from "./datastore";
 import {RestEndpoints} from "./rest-endpoints";
+import {ExternalParameters} from "./external_parameters";
 import {StaticServing} from "./static-serving";
 
 export class RecipesBackend extends GuStack {
@@ -64,7 +67,7 @@ export class RecipesBackend extends GuStack {
 
     const contentUrlBase = this.stage === "CODE" ? "recipes.code.dev-guardianapis.com" : "recipes.guardianapis.com";
 
-    new GuKinesisLambdaExperimental(this, "updaterLambda", {
+    const updaterLambda = new GuKinesisLambdaExperimental(this, "updaterLambda", {
       monitoringConfiguration: {noMonitoring: true},
       existingKinesisStream: {
         externalKinesisStreamName: `content-api-firehose-v2-${this.stage}`,
@@ -111,5 +114,25 @@ export class RecipesBackend extends GuStack {
       fastlyKey: fastlyKeyParam.valueAsString,
       contentUrlBase,
     });
+
+    const durationAlarm = new Alarm(this, "DurationRuntimeAlarm", {
+      alarmDescription: "Notify when the lambda exceeds 75%",
+      actionsEnabled: true,
+      threshold: 22500, //in milliseconds which means 22.5 seconds (which is 75% near to 30 seconds timout of lambda)
+      treatMissingData: TreatMissingData.IGNORE,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
+      metric: updaterLambda.metricDuration({
+        period: Duration.minutes(1),
+        statistic: "Average",
+        unit: Unit.MILLISECONDS// "percent" as we are tyring to use 75 "%" to check against threshold, TODO: Need to check this if it should be Unit.milliseconds to see time wise?
+      }),
+      evaluationPeriods: 3,// when happens atleast 3 times
+    })
+
+    const externalParameters = new ExternalParameters(this, "externals");
+    const urgentAlarmTopic = aws_sns.Topic.fromTopicArn(this, "urgent-alarm", externalParameters.urgentAlarmTopicArn.stringValue);
+    //const nonUrgentAlarmTopic = aws_sns.Topic.fromTopicArn(this, "nonurgent-alarm", externalParameters.nonUrgentAlarmTopicArn.stringValue);
+    durationAlarm.addAlarmAction(new SnsAction(urgentAlarmTopic))
   }
+
 }
