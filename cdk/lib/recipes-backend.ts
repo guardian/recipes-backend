b8689627c9a50af689fdd8d24b1e0ffd7d9917d2
Andy Gallagher <fredex42@gmail.com>
2023-12-05 15:19:41 +0000

    Update cdk/lib/recipes-backend.ts
    Co-authored-by: Jonathon Herbert <jonathon.herbert@guardian.co.uk>

--- cdk/lib/recipes-backend.ts
+++ cdk/lib/recipes-backend.ts
@@ -62,7 +62,7 @@ export class RecipesBackend extends GuStack {
       default: `/${this.stage}/${this.stack}/recipes-responder/fastly-key`
     })
 
-    const contentUrlBase = this.stage=="CODE" ? "recipes.code.dev-guardianapis.com" : "recipes.guardianapis.com";
+    const contentUrlBase = this.stage==="CODE" ? "recipes.code.dev-guardianapis.com" : "recipes.guardianapis.com";
 
     new GuKinesisLambdaExperimental(this, "updaterLambda", {
       monitoringConfiguration: {noMonitoring: true},
