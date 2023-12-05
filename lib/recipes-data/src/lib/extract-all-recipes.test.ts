09dc0b651de8b83bf0e06d0e72d6e58144a088aa
Divya Bhatt <divya.bhatt@guardian.co.uk>
2023-12-05 11:20:16 +0000

    TestCases added for registerMetric and aws-sdk version aligned in package file

--- lib/recipes-data/src/lib/extract-all-recipes.test.ts
+++ lib/recipes-data/src/lib/extract-all-recipes.test.ts
@@ -10,9 +10,8 @@ import {makeCapiDateTime} from "./utils";
 jest.mock("@recipes-api/cwmetrics", () => ({
   registerMetric: jest.fn(),
 }));
-jest.mock("./config", ()=>({
 
-}));
+jest.mock("./config", () => ({}));
 
 jest.mock("./config", () => ({}));
 
