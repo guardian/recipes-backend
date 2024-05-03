78e46e05f16ba9b56321446b68fca0ebff60a2d2
Andy Gallagher <fredex42@gmail.com>
2024-05-03 17:56:54 +0100

    tests for data-receive lambda

---
+++ lambda/facia-responder/src/main.test.ts
@@ -0,0 +1,121 @@
+import {ZodError} from "zod";
+import {importCurationData} from "@recipes-api/lib/recipes-data";
+import {handler} from "./main";
+
+jest.mock("@recipes-api/lib/recipes-data", ()=>({
+  importCurationData: jest.fn()
+}));
+
+const rawContent = {
+  id: "D9AEEA41-F8DB-4FC8-A0DA-275571EA7331",
+  edition: "northern",
+  version: "v1",
+  issueDate: "2024-01-02",
+  fronts: {
+    "all-recipes": [
+      {
+        "id": "d353e2de-1a65-45de-85ca-d229bc1fafad",
+        "title": "Dish of the day",
+        "body": "",
+        "items": [
+          {
+            "recipe": {
+              "id": "14129325"
+            }
+          }
+        ]
+      }
+    ],
+    "meat-free": [
+      {
+        "id": "fa6ccb35-926b-4eff-b3a9-5d0ca88387ff",
+        "title": "Dish of the day",
+        "body": "",
+        "items": [
+          {
+            "recipe": {
+              "id": "14132263"
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+describe("main", ()=>{
+  it("should publish the content it was given", async ()=>{
+    const rec = {
+      Records: [
+        {
+          eventSource: "sqs",
+          awsRegion: "xx-north-n",
+          messageId: "BDB66A64-F095-4F4D-9B6A-135173E262A5",
+          body: JSON.stringify(rawContent)
+        }
+      ]
+    };
+
+    // @ts-ignore
+    await handler(rec, null, null);
+
+    const importCurationDataMock = importCurationData as jest.Mock;
+    expect(importCurationDataMock.mock.calls.length).toEqual(2);
+    expect(importCurationDataMock.mock.calls[0][0]).toEqual(JSON.stringify(rawContent.fronts["all-recipes"]));
+    expect(importCurationDataMock.mock.calls[0][1]).toEqual(rawContent.edition);
+    expect(importCurationDataMock.mock.calls[0][2]).toEqual("all-recipes");
+    expect(importCurationDataMock.mock.calls[0][3]).toEqual(new Date(2024, 0, 2));
+
+    expect(importCurationDataMock.mock.calls[1][0]).toEqual(JSON.stringify(rawContent.fronts["meat-free"]));
+    expect(importCurationDataMock.mock.calls[1][1]).toEqual(rawContent.edition);
+    expect(importCurationDataMock.mock.calls[1][2]).toEqual("meat-free");
+    expect(importCurationDataMock.mock.calls[1][3]).toEqual(new Date(2024, 0, 2));
+  });
+
+
+  it("should not accept valid json that does not match schema", async ()=>{
+    const brokenContent = rawContent;
+    brokenContent.issueDate = "dfsdfsjk";
+    const rec = {
+      Records: [
+        {
+          eventSource: "sqs",
+          awsRegion: "xx-north-n",
+          messageId: "BDB66A64-F095-4F4D-9B6A-135173E262A5",
+          body: JSON.stringify(brokenContent)
+        }
+      ]
+    };
+
+    const expectedError = new ZodError([
+      {
+        "code": "custom",
+        "fatal": true,
+        "path": [
+          "issueDate"
+        ],
+        "message": "Invalid input"
+      }
+    ])
+
+    // @ts-ignore
+    await expect(()=>handler(rec, null, null)).rejects.toEqual(expectedError);
+  });
+
+  it("should not accept invalid json", async ()=>{
+    const rec = {
+      Records: [
+        {
+          eventSource: "sqs",
+          awsRegion: "xx-north-n",
+          messageId: "BDB66A64-F095-4F4D-9B6A-135173E262A5",
+          body: "blahblahblahthisisnotjson"
+        }
+      ]
+    };
+
+    const expectedError = new SyntaxError("Unexpected token b in JSON at position 0");
+    // @ts-ignore
+    await expect(()=>handler(rec, null, null)).rejects.toEqual(expectedError);
+  })
+})
