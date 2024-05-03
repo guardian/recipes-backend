3672ce17f04c6ff45df3ba8774e026aadfe6469c
Andy Gallagher <fredex42@gmail.com>
2024-05-03 16:17:27 +0100

    adding data models from Facia

---
+++ lib/facia/src/lib/facia-models.ts
@@ -0,0 +1,56 @@
+import { z } from "zod";
+
+//This should mirror FeastAppModel in Facia backend
+export const RecipeIdentifier = z.object({
+  id: z.string()
+})
+
+export type RecipeIdentifier = z.infer<typeof RecipeIdentifier>;
+
+export const Recipe = z.object({
+  recipe: RecipeIdentifier
+});
+
+export type Recipe = z.infer<typeof Recipe>;
+
+export const Chef = z.object({
+  backgroundHex: z.string().optional(),
+  id: z.string(),
+  image: z.string().optional(),
+  bio: z.string(),                //FIXME - should this be Optional? Check with Basecamp team
+  foregroundHex: z.string().optional()
+});
+
+export type Chef = z.infer<typeof Chef>;
+
+export const Palette = z.object({
+  backgroundHex: z.string().optional(),
+  foregroundHex: z.string().optional(),
+});
+
+export type Palette = z.infer<typeof Chef>;
+
+export const SubCollection = z.object({
+  byline: z.string().optional(),
+  darkPalette: Palette.optional(),
+  image: z.string().optional(),
+  title: z.string(),
+  lightPalette: Palette.optional(),
+  recipes: z.array(z.string())
+});
+
+export type SubCollection = z.infer<typeof SubCollection>;
+
+export type ContainerItem = SubCollection | Chef | Recipe;
+
+export const FeastAppContainer = z.object({
+  id: z.string(),
+  title: z.string(),
+  body: z.string().optional(),
+  items: z.array(z.union([SubCollection, Chef, Recipe]))
+});
+
+export type FeastAppContainer = z.infer<typeof FeastAppContainer>;
+
+export const FeastCuration = z.record(z.string(), z.array(FeastAppContainer));
+export type FeastCuration = z.infer<typeof FeastCuration>;
