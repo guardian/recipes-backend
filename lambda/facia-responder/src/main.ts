6b1f71f30d42d4dc2439ae8525f2bc1f88b02ad3
Andy Gallagher <fredex42@gmail.com>
2024-05-03 17:24:05 +0100

    wire in actual deployment

--- lambda/facia-responder/src/main.ts
+++ lambda/facia-responder/src/main.ts
@@ -1,9 +1,32 @@
-import {SQSHandler} from "aws-lambda";
-import  * as test  from "@recipes-api/lib/facia";
+import type {SQSHandler, SQSRecord} from "aws-lambda";
+import format from "date-fns/format";
+import  * as facia  from "@recipes-api/lib/facia";
+import {importNewData} from "./submit-data";
+
+function parseMesssage(from:SQSRecord):facia.FeastCuration
+{
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- not unsafe because we validate in the next line
+  const parsedContent = JSON.parse(from.body);  // will throw if the content is not valid;
+  return facia.FeastCuration.parse(parsedContent);
+}
+
+async function deployCuration(curation:facia.FeastCuration)
+{
+  const issueDate = new Date(curation.issueDate);
+  for(const frontName of Object.keys(curation.fronts)) {
+    console.log(`Deploying new front for ${frontName} in ${curation.edition as string} on ${format(issueDate, "YYYY-mm-dd")}`);
+    const serializedFront = JSON.stringify(curation.fronts[frontName]);
+    await importNewData(serializedFront, curation.edition, frontName, issueDate);
+  }
+}
 
 export const handler:SQSHandler = async (event)=> {
   for(const rec of event.Records) {
     console.log(`Received message with ID ${rec.messageId}`);
 
+    //If something fails here, let it crash. The message will get retried and then sent to DLQ
+    // by the Lambda runtime and we will continue running
+    const newCuration = parseMesssage(rec);
+    await deployCuration(newCuration);
   }
 }
--- lib/facia/src/lib/facia-models.test.ts
+++ lib/facia/src/lib/facia-models.test.ts
@@ -3,8 +3,13 @@ import {FeastCuration} from './facia-models';
 
 describe("facia-models", ()=>{
   it("should be able to validate real data", ()=>{
-    const rawContent ={
-      "all-recipes": [
+    const rawContent = {
+      id: "D9AEEA41-F8DB-4FC8-A0DA-275571EA7331",
+      edition: "northern",
+      version: "v1",
+      issueDate: "2024-01-02",
+      fronts: {
+        "all-recipes": [
         {
           "id": "d353e2de-1a65-45de-85ca-d229bc1fafad",
           "title": "Dish of the day",
@@ -18,7 +23,7 @@ describe("facia-models", ()=>{
           ]
         }
       ],
-      "meat-free": [
+        "meat-free": [
         {
           "id": "fa6ccb35-926b-4eff-b3a9-5d0ca88387ff",
           "title": "Dish of the day",
@@ -33,15 +38,16 @@ describe("facia-models", ()=>{
         }
       ]
     }
+  }
 
     const typedData = FeastCuration.parse(rawContent);
-    expect(typedData["all-recipes"].length).toEqual(1);
+    expect(typedData.fronts["all-recipes"].length).toEqual(1);
 
-    expect(typedData["all-recipes"][0].body).toEqual("");
-    expect(typedData["all-recipes"][0].id).toEqual("d353e2de-1a65-45de-85ca-d229bc1fafad");
-    expect(typedData["all-recipes"][0].title).toEqual("Dish of the day");
-    expect(typedData["all-recipes"][0].items.length).toEqual(1);
-    const theRecipe = typedData["all-recipes"][0].items[0] as Recipe;
+    expect(typedData.fronts["all-recipes"][0].body).toEqual("");
+    expect(typedData.fronts["all-recipes"][0].id).toEqual("d353e2de-1a65-45de-85ca-d229bc1fafad");
+    expect(typedData.fronts["all-recipes"][0].title).toEqual("Dish of the day");
+    expect(typedData.fronts["all-recipes"][0].items.length).toEqual(1);
+    const theRecipe = typedData.fronts["all-recipes"][0].items[0] as Recipe;
     expect(theRecipe.recipe.id).toEqual("14129325")
 
 });
