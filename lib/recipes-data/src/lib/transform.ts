3f4ae55a1ce87e18d589f86ff95fc52e683a2212
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-07-22 11:21:16 +0100

    Reapply "Merge pull request #43 from guardian/jsh/add-quality-to-param"
    This reverts commit 35edacdae03b37801a5e478b41249b6b9af8988e.

--- lib/recipes-data/src/lib/transform.ts
+++ lib/recipes-data/src/lib/transform.ts
@@ -6,30 +6,50 @@ import {extractCropDataFromGuimUrl} from './utils';
 
 export type RecipeTransformationFunction = (recipeData: Record<string, unknown>) => Record<string, unknown>;
 
-const getFastlyUrl = (
-  imageId: string,
-  cropId: string,
-  dpr: number,
-  desiredWidth: number,
-  originalWidth: number,
-  extension: string
-) => `https://i.guim.co.uk/img/media/${imageId}/${cropId}/master/${originalWidth}.${extension}?width=${desiredWidth}&dpr=${dpr}&s=none`;
+const getFastlyUrl = ({
+	imageId,
+	cropId,
+	dpr,
+	desiredWidth,
+	originalWidth,
+	extension,
+}: {
+	imageId: string;
+	cropId: string;
+	dpr: number;
+	desiredWidth: number;
+	originalWidth: number;
+	extension: string;
+}) =>
+	`https://i.guim.co.uk/img/media/${imageId}/${cropId}/master/${originalWidth}.${extension}?width=${desiredWidth}&dpr=${dpr}&s=none`;
 
-const getFastlyTemplateUrl = (
-	imageId: string,
-	cropId: string,
-	dpr: number,
-	originalWidth: number,
-  extension: string
-) => `https://i.guim.co.uk/img/media/${imageId}/${cropId}/master/${originalWidth}.${extension}?width=#{width}&dpr=${dpr}&s=none`;
+const getFastlyTemplateUrl = ({
+	imageId,
+	cropId,
+	dpr,
+	originalWidth,
+	extension,
+}: {
+	imageId: string;
+	cropId: string;
+	dpr: number;
+	originalWidth: number;
+	extension: string;
+}) =>
+	`https://i.guim.co.uk/img/media/${imageId}/${cropId}/master/${originalWidth}.${extension}?width=#{width}&quality=#{quality}&dpr=${dpr}&s=none`;
 
-export const replaceFastlyUrl = (
-  recipeId: string,
-  image: RecipeImage,
-  desiredWidth: number,
-  dpr: number,
-): RecipeImage => {
-  const cropData = extractCropDataFromGuimUrl(image.url);
+export const replaceFastlyUrl = ({
+	recipeId,
+	image,
+	desiredWidth,
+	dpr,
+}: {
+	recipeId: string;
+	image: RecipeImage;
+	desiredWidth: number;
+	dpr: number;
+}): RecipeImage => {
+	const cropData = extractCropDataFromGuimUrl(image.url);
 
   if (!cropData) {
     console.warn(
@@ -42,8 +62,21 @@ export const replaceFastlyUrl = (
 
 	return {
 		...image,
-		url: getFastlyUrl(mediaId, cropId, dpr, desiredWidth, width, extension),
-		templateUrl: getFastlyTemplateUrl(mediaId, cropId, dpr, width, extension),
+		url: getFastlyUrl({
+			imageId: mediaId,
+			cropId,
+			dpr,
+			desiredWidth,
+			originalWidth: width,
+			extension,
+		}),
+		templateURL: getFastlyTemplateUrl({
+			imageId: mediaId,
+			cropId,
+			dpr,
+			originalWidth: width,
+			extension,
+		}),
 	};
 };
 
@@ -99,31 +132,31 @@ export const replaceImageUrlsWithFastly = <R extends Record<string, unknown> & R
     return recipe;
   }
 
-  try {
-    return {
-      ...recipe,
-      previewImage: replaceFastlyUrl(
-        recipe.id,
-        recipe.previewImage ?? recipe.featuredImage,
-        PreviewImageWidth,
-        ImageDpr,
-      ),
-      featuredImage: replaceFastlyUrl(
-        recipe.id,
-        recipe.featuredImage,
-        FeaturedImageWidth,
-        ImageDpr,
-      ),
-    };
-  } catch (err) {
-    if (err instanceof Error) {
-      console.warn(
-        `Could not replace preview image url with fastly resizer url for recipe ${recipe.id} - ${err.message}`,
-        err,
-      );
-    }
-    return recipe;
-  }
+	try {
+		return {
+			...recipe,
+			previewImage: replaceFastlyUrl({
+				recipeId: recipe.id,
+				image: recipe.previewImage ?? recipe.featuredImage,
+				desiredWidth: PreviewImageWidth,
+				dpr: ImageDpr,
+    }),
+			featuredImage: replaceFastlyUrl({
+				recipeId: recipe.id,
+				image: recipe.featuredImage,
+				desiredWidth: FeaturedImageWidth,
+				dpr: ImageDpr,
+    }),
+		};
+	} catch (err) {
+		if (err instanceof Error) {
+			console.warn(
+				`Could not replace preview image url with fastly resizer url for recipe ${recipe.id} - ${err.message}`,
+				err,
+			);
+		}
+		return recipe;
+	}
 };
 
 /**
