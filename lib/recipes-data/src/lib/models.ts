6feef7f41981b4ad2aea4aaa82131d125b0b1d40
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-07-22 11:20:40 +0100

    Reapply "Merge pull request #38 from guardian/template-url"
    This reverts commit 753a0e6027361ae7ede46c2c28ad2f50fcb97109.

--- lib/recipes-data/src/lib/models.ts
+++ lib/recipes-data/src/lib/models.ts
@@ -121,6 +121,7 @@ export function recipeReferenceFromIndexEntry(entry: RecipeIndexEntry, jsonBlob:
 
 export type RecipeImage = {
 	url: string;
+  templateUrl?: string; // Contains #{width} so that device can request image at needed size
 	mediaId?: string;
 	cropId?: string;
 	source?: string;
