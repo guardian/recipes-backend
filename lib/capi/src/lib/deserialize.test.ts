0a4330e86e62f8ae78f437f228a708a9b2c4d296
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2024-09-17 10:54:33 +0100

    Remove unneeded async

--- lib/capi/src/lib/deserialize.test.ts
+++ lib/capi/src/lib/deserialize.test.ts
@@ -4,7 +4,7 @@ import { ItemType } from '@guardian/content-api-models/crier/event/v1/itemType';
 import { deserializeEvent } from './deserialize';
 
 describe('deserializeEvent', () => {
-	it('should deserialize a test data blob', async () => {
+	it('should deserialize a test data blob', () => {
 		const b64data = fs.readFileSync('failing-event.b64');
 		const blob = atob(b64data.toString('latin1').trim());
 		const result = deserializeEvent(blob);
