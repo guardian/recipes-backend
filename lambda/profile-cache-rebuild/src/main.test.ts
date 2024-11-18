28e7ddbc658b7cdb0704184055e79e2fca48ad88
Andy Gallagher <fredex42@gmail.com>
2024-11-18 16:05:40 +0000

    fix tests

--- lambda/profile-cache-rebuild/src/main.test.ts
+++ lambda/profile-cache-rebuild/src/main.test.ts
@@ -3,6 +3,8 @@ import { TagType } from '@guardian/content-api-models/v1/tagType';
 import type { ChefInfoFile } from '@recipes-api/lib/recipes-data';
 import { buildChefInfo } from './main';
 
+jest.mock('./config', () => ({}));
+
 describe('buildChefInfo', () => {
 	it('should build up a simplified structure from the data we provide', () => {
 		const incoming: Tag[] = [
