eb64963c84a4e39029fe24d5850386ba87f93067
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-12-19 14:15:43 +0000

    Avoid default export with * for test fixture

--- lambda/recipes-reindex/src/writeBatchToReindexQueue/index.test.ts
+++ lambda/recipes-reindex/src/writeBatchToReindexQueue/index.test.ts
@@ -1,7 +1,7 @@
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import type { Callback, Context } from 'aws-lambda';
 import { mockClient } from 'aws-sdk-client-mock';
-import * as indexJSON from './fixtures/index.json';
+import indexJSON from './fixtures/index.json';
 import { writeBatchToReindexQueueHandler } from './index';
 
 const RecipeIndexSnapshotBucket = 'example-reindex-bucket';
