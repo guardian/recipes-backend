c1c9cc19fe9f4eeb6bb6e7ade2a87bb956b987a9
Andy Gallagher <fredex42@gmail.com>
2025-04-04 11:40:43 +0100

    bugfixes and more tests

--- lambda/rest-endpoints/src/curation.test.ts
+++ lambda/rest-endpoints/src/curation.test.ts
@@ -1,8 +1,5 @@
 import { Readable } from 'stream';
-import type {
-	GetObjectCommand,
-	GetObjectCommandInput,
-} from '@aws-sdk/client-s3';
+import type { GetObjectCommandInput } from '@aws-sdk/client-s3';
 import { S3Client } from '@aws-sdk/client-s3';
 import { mockClient } from 'aws-sdk-client-mock';
 import type { FeastAppContainer } from '@recipes-api/lib/facia';
@@ -66,11 +63,104 @@ describe('generateHybridFront', () => {
 			2,
 			new Date(2025, 0, 2),
 		);
-		console.log(JSON.stringify(result));
+
 		expect(result[0].title).toEqual('container 1');
 		expect(result[1].title).toEqual('container 2');
 		expect(result[2].title).toEqual('inserted container');
 		expect(result[3].title).toEqual('container 3');
 		expect(result[4].title).toEqual('container 4');
+	});
+
+	it('should safely handle the insertion point being beyond the end of the curated front', async () => {
+		const mainCurationBody = Readable.from(
+			Buffer.from(JSON.stringify(mockMainCurationData)),
+		);
+		const insertBody = Readable.from(
+			Buffer.from(JSON.stringify(mockInsertCurationData)),
+		);
+
+		s3Mock.callsFake((input: GetObjectCommandInput) => {
+			console.log(JSON.stringify(input));
+		});
+
+		s3Mock
+			.onAnyCommand({
+				Bucket: undefined,
+				Key: `northern/all-recipes/curation.json`,
+			})
+			.resolves({
+				// @ts-ignore
+				Body: mainCurationBody,
+			});
+
+		s3Mock
+			.onAnyCommand({
+				Bucket: undefined,
+				Key: `dynamic/curation/2025-01-02/FR.json`,
+			})
+			.resolves({
+				//@ts-ignore
+				Body: insertBody,
+			});
+
+		const result = await generateHybridFront(
+			'northern',
+			'all-recipes',
+			'fr',
+			100,
+			new Date(2025, 0, 2),
+		);
+
+		expect(result[0].title).toEqual('container 1');
+		expect(result[1].title).toEqual('container 2');
+		expect(result[2].title).toEqual('container 3');
+		expect(result[3].title).toEqual('container 4');
+		expect(result[4].title).toEqual('inserted container');
+	});
+
+	it('should not perform insertion for meat-free (at present)', async () => {
+		const mainCurationBody = Readable.from(
+			Buffer.from(JSON.stringify(mockMainCurationData)),
+		);
+		const insertBody = Readable.from(
+			Buffer.from(JSON.stringify(mockInsertCurationData)),
+		);
+
+		s3Mock.callsFake((input: GetObjectCommandInput) => {
+			console.log(JSON.stringify(input));
+		});
+
+		s3Mock
+			.onAnyCommand({
+				Bucket: undefined,
+				Key: `northern/meat-free/curation.json`,
+			})
+			.resolves({
+				// @ts-ignore
+				Body: mainCurationBody,
+			});
+
+		s3Mock
+			.onAnyCommand({
+				Bucket: undefined,
+				Key: `dynamic/curation/2025-01-02/FR.json`,
+			})
+			.resolves({
+				//@ts-ignore
+				Body: insertBody,
+			});
+
+		const result = await generateHybridFront(
+			'northern',
+			'meat-free',
+			'fr',
+			2,
+			new Date(2025, 0, 2),
+		);
+
+		expect(result[0].title).toEqual('container 1');
+		expect(result[1].title).toEqual('container 2');
+		expect(result[2].title).toEqual('container 3');
+		expect(result[3].title).toEqual('container 4');
 	});
 });
