da989e8d2184844a9edb4cae5b2a19ec8eab0bf6
Andy Gallagher <fredex42@gmail.com>
2025-04-16 10:43:55 +0100

    ensure that we don't duplicate recipes from the curation into the displayed localised container

--- lambda/rest-endpoints/src/curation.test.ts
+++ lambda/rest-endpoints/src/curation.test.ts
@@ -19,15 +19,31 @@ describe('generateHybridFront', () => {
 	});
 
 	const mockMainCurationData: FeastAppContainer[] = [
-		{ title: 'container 1', items: [] },
+		{
+			title: 'container 1',
+			items: [{ recipe: { id: 'recipe-1a' } }, { recipe: { id: 'recipe-1b' } }],
+		},
 		{ title: 'container 2', items: [] },
-		{ title: 'container 3', items: [] },
-		{ title: 'container 4', items: [] },
+		{
+			title: 'container 3',
+			items: [{ recipe: { id: 'recipe-3a' } }, { recipe: { id: 'recipe-3b' } }],
+		},
+		{
+			title: 'container 4',
+			items: [{ recipe: { id: 'recipe-4a' } }, { recipe: { id: 'recipe-4b' } }],
+		},
 	];
 
 	const mockInsertCurationData: FeastAppContainer = {
 		title: 'inserted container',
-		items: [],
+		items: [
+			{ recipe: { id: 'recipe-5' } },
+			{ recipe: { id: 'recipe-6' } },
+			{ recipe: { id: 'recipe-7' } },
+			{ recipe: { id: 'recipe-1b' } },
+			{ recipe: { id: 'recipe-8' } },
+			{ recipe: { id: 'recipe-9' } },
+		],
 	};
 
 	it('should retrieve both the curated front and inserts and combine them', async () => {
@@ -168,6 +184,57 @@ describe('generateHybridFront', () => {
 		expect(result[1].title).toEqual('container 2');
 		expect(result[2].title).toEqual('container 3');
 		expect(result[3].title).toEqual('container 4');
+	});
+
+	it('should not put recipes in the insert that also exist in the curated content', async () => {
+		const mainCurationBody = Readable.from(
+			Buffer.from(JSON.stringify(mockMainCurationData)),
+		);
+		const insertBody = Readable.from(
+			Buffer.from(JSON.stringify(mockInsertCurationData)),
+		);
+
+		s3Mock.callsFake((input: GetObjectCommandInput) => {
+			console.log(JSON.stringify(input));
+		});
+
+		s3Mock
+			.onAnyCommand({
+				Bucket: 'test',
+				Key: `northern/all-recipes/curation.json`,
+			})
+			.resolves({
+				// @ts-ignore
+				Body: mainCurationBody,
+			});
+
+		s3Mock
+			.onAnyCommand({
+				Bucket: 'test',
+				Key: `dynamic/curation/2025-01-02/FR.json`,
+			})
+			.resolves({
+				//@ts-ignore
+				Body: insertBody,
+			});
+
+		const result = await generateHybridFront(
+			'northern',
+			'all-recipes',
+			'fr',
+			2,
+			new Date(2025, 0, 2),
+		);
+		console.log(JSON.stringify(result));
+
+		expect(result[0].title).toEqual('container 1');
+		expect(result[1].title).toEqual('container 2');
+		expect(result[2].title).toEqual('inserted container');
+		expect(result[2].items.length).toEqual(
+			mockInsertCurationData.items.length - 1,
+		);
+		expect(result[3].title).toEqual('container 3');
+		expect(result[4].title).toEqual('container 4');
 	});
 
 	it("should not crash if the insert data can't be loaded", async () => {
