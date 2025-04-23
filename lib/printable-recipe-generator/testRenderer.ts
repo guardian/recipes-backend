33e4043b364650d92ef1b9157ad76f7241d95475
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-04-23 16:22:41 +0100

    delete from lib folder as we dont need that approach

--- lib/printable-recipe-generator/testRenderer.ts
+++
@@ -1,9 +0,0 @@
-import { JsonToHtmlRenderer } from './jsonToHtmlRender';
-// @ts-ignore
-//import { sampleRecipe } from './sampleRecipeTest.json';
-import sampleRecipe from './sampleRecipeTest.json' assert { type: 'json' };
-import fs from 'fs';
-
-const resultSampleHtml = JsonToHtmlRenderer(sampleRecipe);
-fs.writeFileSync('recipe-output.html', resultSampleHtml);
-console.log('HTML written to recipe-output.html');
