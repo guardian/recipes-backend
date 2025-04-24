074642b4644c17116577a5e86648914c06c7e9f6
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-04-24 15:08:40 +0100

    add routing to make it work on browser too

--- printable-recipe-generator/apps/printable-recipe-generator/src/main.ts
+++ printable-recipe-generator/apps/printable-recipe-generator/src/main.ts
@@ -1,43 +1,64 @@
 import path from 'path';
 import fs from 'fs';
 import ejs from 'ejs';
+import express from 'express';
+
 // @ts-ignore
 //import data from '../src/data/sampleRecipe.json';
 
-//load recipe JSON
-const recipeDataPath = path.join(
-  process.cwd(),
-  'apps',
-  '/printable-recipe-generator',
-  'src',
-  'data',
-  'sampleRecipe.json'
-);
-const recipe = JSON.parse(fs.readFileSync(recipeDataPath, 'utf-8'));
+function renderJsonToHtml() {
+  //load recipe JSON
+  const recipeDataPath = path.join(
+    process.cwd(),
+    'apps',
+    '/printable-recipe-generator',
+    'src',
+    'data',
+    'sampleRecipe.json'
+  );
+  const recipe = JSON.parse(fs.readFileSync(recipeDataPath, 'utf-8'));
 
-//load template
-const templatePath = path.join(
-  process.cwd(),
-  'apps',
-  '/printable-recipe-generator',
-  'templates',
-  'recipe.ejs'
-);
-const template = fs.readFileSync(templatePath, 'utf-8');
+  //load template
+  const templatePath = path.join(
+    process.cwd(),
+    'apps',
+    '/printable-recipe-generator',
+    'templates',
+    'recipe.ejs'
+  );
+  const template = fs.readFileSync(templatePath, 'utf-8');
 
-//Render html
-let renderHtml = '';
-try {
-  renderHtml = ejs.render(template, recipe);
-} catch (error) {
-  console.error('Failed to render template: ', (error as Error).message);
-  //console.error('Recipe data was: ', JSON.stringify(recipe, null, 2));
-  process.exit(1);
+  //Render html
+  let renderHtml = '';
+  try {
+    renderHtml = ejs.render(template, recipe);
+  } catch (error) {
+    console.error('Failed to render template: ', (error as Error).message);
+    //console.error('Recipe data was: ', JSON.stringify(recipe, null, 2));
+    process.exit(1);
+  }
+
+  //Output
+  const outputPath = path.join(__dirname, 'output', 'recipe.html');
+  fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+  fs.writeFileSync(outputPath, renderHtml);
+
+  console.log(`Rendered HTML saved to ${outputPath}`);
 }
 
-//Output
-const outputPath = path.join(__dirname, 'output', 'recipe.html');
-fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-fs.writeFileSync(outputPath, renderHtml);
+const app = express();
 
-console.log(`Rendered HTML saved to ${outputPath}`);
+app.set('view engine', 'ejs');
+//app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  renderJsonToHtml();
+});
+app.get('/print', (req, res) => {
+  res.sendFile(path.join(__dirname + '/output/recipe.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
