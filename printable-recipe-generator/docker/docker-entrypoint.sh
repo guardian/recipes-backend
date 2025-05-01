d6ce9b175f1503b7e88c9a9102c25a032fbe0109
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-05-01 13:50:22 +0100

    Add html to pdf and copy to s3 to docker.
    Co-authored-by: Andy Gallagher <andy.gallagher@theguardian.com>

--- printable-recipe-generator/docker/docker-entrypoint.sh
+++ printable-recipe-generator/docker/docker-entrypoint.sh
@@ -24,3 +24,15 @@ RECIPE_HTML_OUTPUT="/tmp/recipe.html"
 node /printable-recipe-generator/main.js "$RECIPE_CONTENT_PATH" "$RECIPE_HTML_OUTPUT"
 echo "Recipe html is rendered at $RECIPE_HTML_OUTPUT"
 
+#Convert htmlfile to PDF file
+echo "Take html and convert it to PDF using headless chrome"
+RECIPE_PDF_OUTPUT='/tmp/recipe.pdf'
+google-chrome-stable --headless --disable-gpu --print-to-pdf="$RECIPE_PDF_OUTPUT" "$RECIPE_HTML_OUTPUT"
+echo "Recipe PDF is generated at $RECIPE_PDF_OUTPUT"
+
+#Copy PDF file to S3
+echo "Copy recipe.pdf file to S3 now"
+RECIPE_PDF_S3_OUTPUT="s3://${BUCKET}/content/${RECIPE_CSID}.pdf"
+aws s3 cp "$RECIPE_PDF_OUTPUT" "$RECIPE_PDF_S3_OUTPUT"
+echo "Recipe PDF is uploaded to S3 bucket at $RECIPE_PDF_S3_OUTPUT"
+
