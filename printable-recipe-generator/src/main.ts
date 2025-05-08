c5ce7ef609e95bd1192a6e5885a6ed9b013b4d22
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-05-08 12:23:05 +0100

    Update main.ts to have svgs

--- printable-recipe-generator/src/main.ts
+++ printable-recipe-generator/src/main.ts
@@ -5,6 +5,24 @@ import type { Data } from 'ejs';
 import { render as renderTemplate } from 'ejs';
 
 function renderJsonToHtml(recipeDataPath: string) {
+	//load SVGs
+	const svgPath = (fileName: string) =>
+		path.join(__dirname, 'src', 'assets', 'svgs', fileName);
+
+	const svgs = {
+		FeastLogo: fs.readFileSync(svgPath('FeastLogo.svg'), 'utf-8'),
+		TheGuardianLogo: fs.readFileSync(svgPath('TheGuardianLogo.svg'), 'utf-8'),
+		'dairy-free': fs.readFileSync(svgPath('dairy-free.svg'), 'utf-8'),
+		'gluten-free': fs.readFileSync(svgPath('gluten-free.svg'), 'utf-8'),
+		vegan: fs.readFileSync(svgPath('vegan.svg'), 'utf-8'),
+		vegetarian: fs.readFileSync(svgPath('vegetarian.svg'), 'utf-8'),
+		camera: fs.readFileSync(svgPath('camera.svg'), 'utf-8'),
+		'feast-book-outlined': fs.readFileSync(
+			svgPath('feast-book-outlined.svg'),
+			'utf-8',
+		),
+	};
+
 	//load recipe JSON
 	const recipe = JSON.parse(
 		fs.readFileSync(recipeDataPath, 'utf-8'),
@@ -17,7 +35,7 @@ function renderJsonToHtml(recipeDataPath: string) {
 	//Render html
 	let renderHtml = '';
 	try {
-		renderHtml = renderTemplate(template, recipe as Data);
+		renderHtml = renderTemplate(template, { recipe, svgs } as Data);
 	} catch (error) {
 		console.error('Failed to render template: ', (error as Error).message);
 		//console.error('Recipe data was: ', JSON.stringify(recipe, null, 2));
