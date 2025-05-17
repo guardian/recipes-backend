2cf0763c23cc976b761bdde7e8352799c7cf8121
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-05-17 18:01:45 +0100

    load assets in main.test as well. create ChefData interface and fake data. and add more tests.

--- printable-recipe-generator/src/tests/main.test.ts
+++ printable-recipe-generator/src/tests/main.test.ts
@@ -1,71 +1,70 @@
 import fs from 'fs';
 import path from 'path';
 import * as ejs from 'ejs';
+import { fontsBase64, svgs } from '../assets/assetloader';
 import recipe from '../data/sampleRecipe.json';
 
 //load Contributors
-async function getChefs(): Promise<unknown> {
-	const resp = await fetch(
-		'https://recipes.guardianapis.com/v2/contributors.json',
-	);
-	try {
-		return resp.json().catch(console.error);
-	} catch (error) {
-		console.error('Failed to parse chefs JSON: ', error);
-		return null;
-	}
+interface ChefData {
+	webTitle: string;
+	webUrl: string;
+	apiUrl: string;
+	bio?: string;
+	bylineImageUrl?: string;
+	bylineLargeImageUrl?: string;
 }
 
-//load SVGs
-const svgPath = (fileName: string) =>
-	path.join(__dirname, '../assets/svgs/', fileName);
-
-const svgs = {
-	FeastLogo: fs.readFileSync(svgPath('FeastLogo.svg'), 'utf-8'),
-	TheGuardianLogo: fs.readFileSync(svgPath('TheGuardianLogo.svg'), 'utf-8'),
-	'dairy-free': fs.readFileSync(svgPath('Dairy-free.svg'), 'utf-8'),
-	'gluten-free': fs.readFileSync(svgPath('Gluten-free.svg'), 'utf-8'),
-	vegan: fs.readFileSync(svgPath('Vegan.svg'), 'utf-8'),
-	vegetarian: fs.readFileSync(svgPath('Vegetarian.svg'), 'utf-8'),
-	camera: fs.readFileSync(svgPath('camera.svg'), 'utf-8'),
-	'feast-book-outlined': fs.readFileSync(
-		svgPath('feast-book-outlined.svg'),
-		'utf-8',
-	),
+const chefs: Record<string, ChefData> = {
+	'profile/nigelslater': {
+		webTitle: 'Nigel Slater',
+		webUrl: 'http://www.code.dev-theguardian.com/profile/nigelslater',
+		apiUrl: 'http://content.code.dev-guardianapis.com/profile/nigelslater',
+		bio: "<p>Nigel Slater has been the Observer's food writer for 20 years. His cookery books, which include Appetite, Eat and the Kitchen Diaries have won a host of awards, while his autobiography Toast – The Story of a Boy's Hunger was adapted by BBC Films, starring Helena Bonham Carter and Freddie Highmore</p>",
+		bylineImageUrl:
+			'https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/17/1397749337461/NigelSlaterLv2.jpg',
+		bylineLargeImageUrl:
+			'https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/17/1397762419839/NigelSlater.png',
+	},
+	'profile/claireptak': {
+		webTitle: 'Claire Ptak',
+		webUrl: 'http://www.code.dev-theguardian.com/profile/claireptak',
+		apiUrl: 'http://content.code.dev-guardianapis.com/profile/claireptak',
+	},
 };
-
-//load fonts
-const fontPath = (fileName: string) =>
-	path.join(__dirname, '../assets/fonts/', fileName);
-const fontsBase64 = {
-	RegularEgyptianFont: fs
-		.readFileSync(fontPath('GuardianTextEgyptian-Regular.ttf'))
-		.toString('base64'),
-	RegularSansFont: fs
-		.readFileSync(fontPath('GuardianTextSans-Regular.ttf'))
-		.toString('base64'),
-	HeadlineSemiBoldFont: fs
-		.readFileSync(fontPath('GHGuardianHeadline-Semibold.otf'))
-		.toString('base64'),
-	SansBoldFont: fs
-		.readFileSync(fontPath('GuardianTextSans-Bold.ttf'))
-		.toString('base64'),
-};
-
 describe('Sample recipe ', () => {
 	it('should match snapshot', () => {
 		const templatePath = path.join(__dirname, '../assets/recipe.ejs');
 		const template = fs.readFileSync(templatePath, 'utf-8');
-		//Get chefs and render html
-		void (async () => {
-			const chefsList = await getChefs();
-			const html = ejs.render(template, {
-				recipe,
-				svgs,
-				fontsBase64,
-				chefsList,
-			});
-			expect(html).toMatchSnapshot();
-		})();
+		const html = ejs.render(template, {
+			recipe,
+			svgs,
+			fontsBase64,
+			chefs,
+		});
+		expect(html).toMatchSnapshot();
+	});
+
+	it('should match snapshot even when chefs are undefined', () => {
+		const templatePath = path.join(__dirname, '../assets/recipe.ejs');
+		const template = fs.readFileSync(templatePath, 'utf-8');
+		const html = ejs.render(template, {
+			recipe,
+			svgs,
+			fontsBase64,
+			undefined,
+		});
+		expect(html).toMatchSnapshot();
+	});
+
+	it('should match snapshot even when chefs list is null', () => {
+		const templatePath = path.join(__dirname, '../assets/recipe.ejs');
+		const template = fs.readFileSync(templatePath, 'utf-8');
+		const html = ejs.render(template, {
+			recipe,
+			svgs,
+			fontsBase64,
+			chefs: null,
+		});
+		expect(html).toMatchSnapshot();
 	});
 });
