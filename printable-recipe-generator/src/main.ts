4c384947ed185cd1fa7ea04acecdacba04b6e593
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-05-17 18:00:50 +0100

    load assets in main create ChefData interface to avoid ts-lint. provide stage value to hit right endpoint.

--- printable-recipe-generator/src/main.ts
+++ printable-recipe-generator/src/main.ts
@@ -4,19 +4,31 @@ import path from 'path';
 import type { Data } from 'ejs';
 import { render as renderTemplate } from 'ejs';
 import fetch from 'node-fetch';
+import { fontsBase64, svgs } from './assets/assetloader';
+
+const stage: string = process.env['STAGE'] ?? 'CODE';
 
 //load Contributors
-async function getChefs(): Promise<unknown> {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call -- response is expected to be JSON, safe to assign
-	const resp = await fetch(
-		'https://recipes.guardianapis.com/v2/contributors.json',
-	);
+interface ChefData {
+	webTitle: string;
+	webUrl: string;
+	apiUrl: string;
+	bio?: string;
+	bylineImageUrl?: string;
+	bylineLargeImageUrl?: string;
+}
+async function getChefs(): Promise<Record<string, ChefData> | undefined> {
 	try {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access -- returning JSON
-		return resp.json().catch(console.error);
+		const endpoint: string =
+			stage === 'PROD'
+				? 'https://recipes.guardianapis.com/v2/contributors.json'
+				: 'https://recipes.code.dev-guardianapis.com/v2/contributors.json';
+		const resp = await fetch(endpoint);
+		const data = (await resp.json()) as Record<string, ChefData>;
+		return data;
 	} catch (error) {
 		console.error('Failed to parse chefs JSON: ', error);
-		return null;
+		return undefined;
 	}
 }
 
@@ -24,42 +36,6 @@ export function renderJsonToHtml(
 	recipeDataPath: string,
 	chefs: Record<string, unknown>,
 ) {
-	//load SVGs
-	const svgPath = (fileName: string) =>
-		path.join(__dirname, 'src', 'assets', 'svgs', fileName);
-
-	const svgs = {
-		FeastLogo: fs.readFileSync(svgPath('FeastLogo.svg'), 'utf-8'),
-		TheGuardianLogo: fs.readFileSync(svgPath('TheGuardianLogo.svg'), 'utf-8'),
-		'dairy-free': fs.readFileSync(svgPath('Dairy-free.svg'), 'utf-8'),
-		'gluten-free': fs.readFileSync(svgPath('Gluten-free.svg'), 'utf-8'),
-		vegan: fs.readFileSync(svgPath('Vegan.svg'), 'utf-8'),
-		vegetarian: fs.readFileSync(svgPath('Vegetarian.svg'), 'utf-8'),
-		camera: fs.readFileSync(svgPath('camera.svg'), 'utf-8'),
-		'feast-book-outlined': fs.readFileSync(
-			svgPath('feast-book-outlined.svg'),
-			'utf-8',
-		),
-	};
-
-	//load fonts
-	const fontPath = (fileName: string) =>
-		path.join(__dirname, 'src', 'assets', 'fonts', fileName);
-	const fontsBase64 = {
-		RegularEgyptianFont: fs
-			.readFileSync(fontPath('GuardianTextEgyptian-Regular.ttf'))
-			.toString('base64'),
-		RegularSansFont: fs
-			.readFileSync(fontPath('GuardianTextSans-Regular.ttf'))
-			.toString('base64'),
-		HeadlineSemiBoldFont: fs
-			.readFileSync(fontPath('GHGuardianHeadline-Semibold.otf'))
-			.toString('base64'),
-		SansBoldFont: fs
-			.readFileSync(fontPath('GuardianTextSans-Bold.ttf'))
-			.toString('base64'),
-	};
-
 	//load recipe JSON
 	const recipe = JSON.parse(
 		fs.readFileSync(recipeDataPath, 'utf-8'),
@@ -71,8 +47,6 @@ export function renderJsonToHtml(
 
 	//Render html
 	let renderHtml = '';
-	// @ts-expect-error -- we know this
-	console.log(chefs['profile/yotamottolenghi'].webTitle); //TODO to test - it worked here, not sure why it is not available to EJS file
 	try {
 		renderHtml = renderTemplate(template, {
 			recipe,
@@ -103,7 +77,7 @@ if (!process.argv[2]) {
 } else {
 	//Get chefs and render html
 	void (async () => {
-		const chefsList = (await getChefs()) as Record<string, unknown>;
+		const chefsList = (await getChefs()) as Record<string, ChefData>;
 		renderJsonToHtml(process.argv[2], chefsList);
 	})();
 }
