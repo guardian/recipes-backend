283a162f4e587fa94da19317064979923f13a624
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-07-14 13:12:29 +0100

    tidy up

--- cdk/lib/printable-recipe-generator.ts
+++ cdk/lib/printable-recipe-generator.ts
@@ -160,10 +160,6 @@ export class PrintableRecipeGenerator extends Construct {
 							name: 'RECIPE_CSID',
 							value: EventField.fromPath('$.detail.checksum'),
 						},
-						// {
-						// 	name: 'CONTENT',
-						// 	value: EventField.fromPath('$.detail.blob'),
-						// },
 						{
 							name: 'BUCKET',
 							value: `feast-recipes-static-${scope.stage.toLowerCase()}`,
