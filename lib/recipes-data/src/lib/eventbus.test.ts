62deff6f27e8bc6e6cd770eab0f3dd79a99b815a
Alex Dufournet <alex.dufournet@guardian.co.uk>
2025-12-05 14:54:10 +0100

    Publish a version update for all the checksums

--- lib/recipes-data/src/lib/eventbus.test.ts
+++ lib/recipes-data/src/lib/eventbus.test.ts
@@ -33,11 +33,11 @@ describe('announce_new_recipe', () => {
 				sponsorshipCount: 0,
 				recipeV2Blob: {
 					jsonBlob: 'recep-1-content',
-					checksum: 'recep-1-cs',
+					checksum: 'recep-1-cs2',
 				},
 				recipeV3Blob: {
 					jsonBlob: 'recep-1-content',
-					checksum: 'recep-1-cs',
+					checksum: 'recep-1-cs3',
 				},
 			},
 			{
@@ -45,11 +45,11 @@ describe('announce_new_recipe', () => {
 				sponsorshipCount: 0,
 				recipeV2Blob: {
 					jsonBlob: 'recep-2-content',
-					checksum: 'recep-2-cs-updated',
+					checksum: 'recep-2-cs2-updated',
 				},
 				recipeV3Blob: {
 					jsonBlob: 'recep-2-content',
-					checksum: 'recep-2-cs-updated',
+					checksum: 'recep-2-cs3-updated',
 				},
 			},
 		];
@@ -80,32 +80,50 @@ describe('announce_new_recipe', () => {
 				{
 					EventId: 'event-3',
 				},
+				{
+					EventId: 'event-4',
+				},
+				{
+					EventId: 'event-5',
+				},
 			],
 		});
 		const result = await announceNewRecipe(updates, removals, outgoingEventBus);
-		expect(result).toEqual(3);
+		expect(result).toEqual(5);
 
 		expect(mockEbClient.commandCalls(PutEventsCommand).length).toEqual(1);
 		const putCmd = mockEbClient.commandCalls(PutEventsCommand)[0]
 			.firstArg as PutEventsCommand;
 
-		expect(putCmd.input.Entries?.length).toEqual(3);
+		expect(putCmd.input.Entries?.length).toEqual(5);
 
 		const firstEntry = putCmd.input.Entries ? putCmd.input.Entries[0] : {};
 		expect(firstEntry.DetailType).toEqual('recipe-update');
 		expect(firstEntry.Detail).toEqual(
-			`{"blob":"recep-1-content","uid":"recep-1-uid","checksum":"recep-1-cs"}`,
+			`{"blob":"recep-1-content","uid":"recep-1-uid","checksum":"recep-1-cs2"}`,
 		);
 
 		const secondEntry = putCmd.input.Entries ? putCmd.input.Entries[1] : {};
 		expect(secondEntry.DetailType).toEqual('recipe-update');
 		expect(secondEntry.Detail).toEqual(
-			`{"blob":"recep-2-content","uid":"recep-2-uid","checksum":"recep-2-cs-updated"}`,
+			`{"blob":"recep-1-content","uid":"recep-1-uid","checksum":"recep-1-cs3"}`,
 		);
 
 		const thirdEntry = putCmd.input.Entries ? putCmd.input.Entries[2] : {};
-		expect(thirdEntry.DetailType).toEqual('recipe-delete');
+		expect(thirdEntry.DetailType).toEqual('recipe-update');
 		expect(thirdEntry.Detail).toEqual(
+			`{"blob":"recep-2-content","uid":"recep-2-uid","checksum":"recep-2-cs2-updated"}`,
+		);
+
+		const fourthEntry = putCmd.input.Entries ? putCmd.input.Entries[3] : {};
+		expect(fourthEntry.DetailType).toEqual('recipe-update');
+		expect(fourthEntry.Detail).toEqual(
+			`{"blob":"recep-2-content","uid":"recep-2-uid","checksum":"recep-2-cs3-updated"}`,
+		);
+
+		const fifthEntry = putCmd.input.Entries ? putCmd.input.Entries[4] : {};
+		expect(fifthEntry.DetailType).toEqual('recipe-delete');
+		expect(fifthEntry.Detail).toEqual(
 			`{"checksum":"recep-3-cs","uid":"recep-3-uid"}`,
 		);
 
