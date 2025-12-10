4549163eda729fd7090cbcf3c4d9cb42c29b3bf1
Divya Bhatt <divya.bhatt@guardian.co.uk>
2025-12-10 10:17:40 +0000

    changes to caller endpoint too

--- lambda/rest-endpoints/src/app.ts
+++ lambda/rest-endpoints/src/app.ts
@@ -123,7 +123,13 @@ router.get('/api/:region/:variant/hybrid-curation.json', (req, resp) => {
 		'max-age=7200, stale-while-revalidate=300, stale-if-error=14400';
 	//const curationCacheControl = 'no-store'; //while debugging!
 
-	generateHybridFront(req.params.region, req.params.variant, territoryParam, 2)
+	generateHybridFront(
+		req,
+		req.params.region,
+		req.params.variant,
+		territoryParam,
+		2,
+	)
 		.then((front) => {
 			resp
 				.status(200)
