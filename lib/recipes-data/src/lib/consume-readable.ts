fdc8a1f3b671b63f21219af8de6ab8bd157e77fc
Divya Bhatt <divya.bhatt@guardian.co.uk>
2026-03-11 16:02:30 +0000

    optimized consume-Readable

--- lib/recipes-data/src/lib/consume-readable.ts
+++ lib/recipes-data/src/lib/consume-readable.ts
@@ -1,46 +1,13 @@
 import type { Readable } from 'stream';
 
 /**
- * Just consumes the stream to an in-memory buffer.
- * Not very efficient, I admit, but since the data is small i couldn't be bothered
- * with full ndjson streaming
- * @param readable
+ * Consumes a readable stream and concatenates its chunks into a single buffer.
+ * Collecting chunks in an array and concatenating them is an O(n) operation,
+ * compared to repeatedly concatenating buffers, which is O(n²) because Buffer.concat()
+ * always copies and doesn't realloc.
+ * @param readable - The readable stream to consume.
  */
 export async function consumeReadable(readable: Readable): Promise<Buffer> {
-	return new Promise<Buffer>((resolve, reject) => {
-		let mainBuffer: Buffer = Buffer.alloc(0);
-
-		readable.on('readable', () => {
-			let chunk: Buffer | null;
-			// Use a loop to make sure we read all currently available data
-			while (null !== (chunk = readable.read() as Buffer | null)) {
-				mainBuffer = Buffer.concat([mainBuffer, chunk]);
-			}
-		});
-
-		readable.on('end', () => {
-			resolve(mainBuffer);
-		});
-
-		readable.on('error', (err) => {
-			reject(err);
-		});
-	});
-}
-
-/**
- * Consumes a readable stream and concatenates its chunks into a single buffer.
- * This method is efficient for small data streams and ensures the entire content
- * is read into memory.
- * @param readable - The readable stream to consume.
- * @returns A promise that resolves to a buffer containing the stream's content.
- */
-//TODO - this is basically the same as consumeReadable, but more efficient.
-// We should probably just have one of these and rename it to something more generic?? Query to resolve with Andy.G
-
-export async function consumeReadable_dataToBuffer(
-	readable: Readable,
-): Promise<Buffer> {
 	return new Promise<Buffer>((resolve, reject) => {
 		const chunks: Buffer[] = [];
 
